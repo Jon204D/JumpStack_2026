42cfa46a73fbf3fa41a4045fe1e6d37314bdef15
Jonathan R Dameus <126197399+Jon204D@users.noreply.github.com>
2026-07-14 12:37:32 -0400

    Scaffold Spring Boot Maven application

---
+++ ConsoleBankApp/src/main/java/com/collabera/consolebankapp/ConsoleBankApplication.java
@@ -0,0 +1,12 @@
+package com.collabera.consolebankapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsoleBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsoleBankApplication.class, args);
+    }
+}
---
+++ ConsoleBankApp/src/test/java/com/collabera/consolebankapp/ConsoleBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collabera.consolebankapp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ConsoleBankApplicationTests {
+
+    @Test
+    void scaffoldBuilds() {
+        assertTrue(true);
+    }
+}
